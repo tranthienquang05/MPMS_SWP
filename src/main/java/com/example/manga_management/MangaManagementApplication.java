691a3c7571866b0f730476b20c5bf7b4238242d0
tranthienquang05 <tranthienquang05@gmail.com>
2026-06-13 13:17:46 +0700

    Khởi tạo dự án Manga Management

---
+++ src/main/java/com/example/manga_management/MangaManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.manga_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manga_management/MangaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manga_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

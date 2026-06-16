d48e6a289bd722d67a58e53e2ac674e9ef1c0dad
AnhND-1311 <anhplbl2022@gmail.com>
2026-06-16 21:39:08 +0700

    Anh

--- src/main/java/com/example/manga_management/controller/MangakaController.java
+++ src/main/java/com/example/manga_management/controller/MangakaController.java
@@ -1,21 +1,26 @@
 package com.example.manga_management.controller;
 
-import com.example.manga_management.entity.Mangaka;
-import com.example.manga_management.entity.Proposal;
-import com.example.manga_management.entity.User;
-import com.example.manga_management.repository.MangakaRepository;
-import com.example.manga_management.repository.ProposalRepository;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.manga_management.entity.Mangaka;
+import com.example.manga_management.entity.Proposal;
+import com.example.manga_management.entity.User;
+import com.example.manga_management.repository.MangakaRepository;
+import com.example.manga_management.repository.ProposalRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/manga/mangaka")

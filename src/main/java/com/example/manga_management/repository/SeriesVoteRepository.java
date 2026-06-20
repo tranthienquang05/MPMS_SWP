6ad8d10f6971fc9701b245228ed3ff5434546f8b
hao-techsad <haovnase182622@fpt.edu.vn>
2026-06-20 14:29:12 +0700

    Update SeriesVoteRepository.java

--- src/main/java/com/example/manga_management/repository/SeriesVoteRepository.java
+++ src/main/java/com/example/manga_management/repository/SeriesVoteRepository.java
@@ -5,4 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeriesVoteRepository extends JpaRepository<SeriesVote, String> {
     long countBySeries_SeriesID(String seriesID);
+
+    long countBySeries_SeriesIDAndVote(String seriesID, String vote);
+    
+    long countBySeries_SeriesIDAndBoard_BoardID(
+        String seriesID,
+        String boardID
+        );
 }
